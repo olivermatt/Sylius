e4a8cc759a022b9ffa5364dc66a6d11591a340dd
Oliver Matt <olivermatt@live.com>
2022-08-27 00:43:56 +0200

    a

--- src/Payum/ModenaPaymentGatewayFactory.php
+++ src/Payum/ModenaPaymentGatewayFactory.php
@@ -2,18 +2,18 @@
 
 declare(strict_types=1);
 
-namespace ModenaFin\SyliusModenaPlugin;
+namespace ModenaFin\SyliusExamplePlugin\Payum;
 
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayFactory;
 
-final class SyliusPaymentGatewayFactory extends GatewayFactory
+final class ModenaPaymentGatewayFactory extends GatewayFactory
 {
     protected function populateConfig(ArrayObject $config): void
     {
         $config->defaults([
             'payum.factory_name' => 'sylius_payment',
-            'payum.factory_title' => 'Sylius Payment',
+            'payum.factory_title' => 'Modena Payment',
         ]);
 
         $config['payum.api'] = function (ArrayObject $config) {
