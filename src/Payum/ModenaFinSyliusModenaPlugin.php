ea0889479bf719afec4bd2709c3566bec5509226
Oliver Matt <olivermatt@live.com>
2022-09-01 23:10:29 +0200

    dawdaw

--- src/Payum/ModenaFinSyliusModenaPlugin.php
+++ src/Payum/ModenaFinSyliusModenaPlugin.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ModenaFin\SyliusModenaPlugin;
+namespace ModenaFin\SyliusExamplePlugin\Payum;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
