ef9cbd89d9d4c46d1350b8700565e9b25ad46076
Oliver Matt <olivermatt@live.com>
2022-09-02 17:45:58 +0200

    ertfg

--- src/Payum/Action/ModenaAuth.php
+++ src/Payum/Action/ModenaAuth.php
@@ -8,6 +8,13 @@ namespace Acme\SyliusExamplePlugin\Payum\Action;
 class ModenaAuth 
 {
 
+    public function __construct()
+    {
+
+        echo '<script>alert("Modena AUth Called");</script>';
+
+    }
+
     function execute()
     {
         return true;
