1d59bbc9facc67ab43d7d64320c7772677bccaa5
Oliver Matt <olivermatt@live.com>
2022-10-02 16:42:34 +0300

    34

--- src/Payum/SyliusPaymentGatewayFactory.php
+++ src/Payum/SyliusPaymentGatewayFactory.php
@@ -33,7 +33,7 @@ final class SyliusPaymentGatewayFactory extends GatewayFactory
             'payum.factory_name' => 'sylius_payment',
             'payum.factory_title' => 'Sylius Payment',
             'payum.action.capture' => new CaptureAction($i),
-            'payum.action.status' => new StatusAction($i),
+            'payum.action.status' => new StatusAction(),
         ]);
 
         /// 
