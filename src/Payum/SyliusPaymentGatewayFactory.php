2965a8df52397fd0938b3a901b1de5257a344dc2
Oliver Matt <olivermatt@live.com>
2022-10-03 11:33:24 +0300

    we

--- src/Payum/SyliusPaymentGatewayFactory.php
+++ src/Payum/SyliusPaymentGatewayFactory.php
@@ -33,12 +33,13 @@ final class SyliusPaymentGatewayFactory extends GatewayFactory
 
         $config->defaults([
             'payum.factory_name' => 'sylius_payment',
-            'payum.factory_title' => 'Sylius Payment',
+            'payum.factory_title' => 'Modena Payment',
             'payum.action.capture' => new CaptureAction($client_id, $client_secret, $product),
             'payum.action.status' => new StatusAction(),
         ]);
 
-        /// 
+        /// 'payum.factory_title' => 'Sylius Payment',
+
 
 
           $config['payum.api'] = function (ArrayObject $config) {
