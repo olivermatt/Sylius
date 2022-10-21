5de500768b1e85f1bc9e2a66a37e3669f24223b7
Oliver Matt <olivermatt@live.com>
2022-10-21 16:16:08 +0300

    we

--- src/Payum/Lib/ModenaPaymentManager.php
+++ src/Payum/Lib/ModenaPaymentManager.php
@@ -98,8 +98,6 @@ class ModenaPaymentManager extends Generic
         $request['cancelUrl'] = "https://google.com"; 
         $request['callbackUrl'] = $this->return_url;
 
-        $log->warning('Request: ' . json_encode($request));
-
         return json_encode($request);
     }
 
@@ -161,6 +159,8 @@ class ModenaPaymentManager extends Generic
         */
 
         $this->modena_redirect_url = $redirect_url;
+        $log->warning('Redirurl 2: ' . $r->location);
+
         $log->warning('Redir url: ' . $redirect_url);
         $log->warning('Create Order resp status: ' . $statusCode);
         
