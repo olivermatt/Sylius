ac79947e79c4371993d955aa547905ef7dadf0a7
Oliver Matt <olivermatt@live.com>
2022-10-24 12:13:43 +0300

    d

---
+++ src/AcmeSyliusExamplePlugin.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\SyliusExamplePlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AcmeSyliusExamplePlugin extends Bundle
+{
+
+}
+
