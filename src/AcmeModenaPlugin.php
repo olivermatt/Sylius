68a44d63a9e7a5c4b64b1d326e0c7cae16942f67
Oliver Matt <olivermatt@live.com>
2022-10-26 17:32:19 +0300

    wew

---
+++ src/AcmeModenaPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\ModenaPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+///final class AcmeSyliusExamplePlugin extends Bundle
+
+final class AcmeModenaPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
+
