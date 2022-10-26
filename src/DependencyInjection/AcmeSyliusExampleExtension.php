cdeb65d93177b76b8c1414d6f4a5035190da5eb0
Oliver Matt <olivermatt@live.com>
2022-10-26 13:24:57 +0300

    w

--- src/DependencyInjection/AcmeSyliusExampleExtension.php
+++ src/DependencyInjection/AcmeSyliusExampleExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExamplePluginExtension extends Extension
+final class AcmeSyliusExampleExtension extends Extension
 {
     /**
      * @psalm-suppress UnusedVariable
