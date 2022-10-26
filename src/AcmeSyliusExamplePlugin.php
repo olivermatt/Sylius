e56d27db847e8242931985f0b0029ba0f542c771
Oliver Matt <olivermatt@live.com>
2022-10-26 13:07:57 +0300

    we

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 ///namespace Acme\SyliusExamplePlugin;
-namespace Acme\SyliusExamplePlugin;
+   namespace Acme\SyliusExamplePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
