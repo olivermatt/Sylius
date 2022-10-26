d37c90660097b5adde60bcf2156e7126e31035a7
Oliver Matt <olivermatt@live.com>
2022-10-26 10:38:07 +0300

    acmeplgin

--- src/Form/Type/ModenaGatewayConfigurationType.php
+++ src/Form/Type/ModenaGatewayConfigurationType.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
+          
 namespace Acme\ModenaPlugin\Form\Type;
 ///namespace Acme\SyliusExamplePlugin\Form\Type;
 
@@ -9,7 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-
+            
 final class ModenaGatewayConfigurationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
