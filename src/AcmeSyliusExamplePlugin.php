9eab7cc09b449df7ac5074c9763d91a90ed81e71
Oliver Matt <olivermatt@live.com>
2022-10-26 13:17:56 +0300

    wd

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-///namespace Acme\SyliusExamplePlugin;
-   namespace Acme\SyliusExamplePlugin;
+namespace Acme\SyliusExamplePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\ModenaPlugin\DependencyInjection;
+namespace Acme\SyliusExamplePlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('acme_modena_plugin');
+        $treeBuilder = new TreeBuilder('acme_sylius_example_plugin');
         $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
