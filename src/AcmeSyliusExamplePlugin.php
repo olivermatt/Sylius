4037599c0218087c727265518a3dbcbdf79911f5
Oliver Matt <olivermatt@live.com>
2022-10-26 21:14:23 +0300

    changes

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Acme\SyliusExamplePlugin99;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 ///final class AcmeSyliusExamplePlugin extends Bundle
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class AcmeSyliusExamplePlugin99 extends Bundle
 {
     use SyliusPluginTrait;
 }
